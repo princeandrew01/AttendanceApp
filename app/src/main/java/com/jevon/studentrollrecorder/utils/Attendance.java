e908ae43dd72797acf56d4831fc04978b9c5ef82
NikiSmalls <43125254+NikiSmalls@users.noreply.github.com>
2018-12-16 17:36:27 -0400

    Adding comments and removing unneccesary code

--- app/src/main/java/com/jevon/studentrollrecorder/utils/Attendance.java
+++ app/src/main/java/com/jevon/studentrollrecorder/utils/Attendance.java
@@ -1,21 +1,20 @@
 package com.jevon.studentrollrecorder.utils;
+/**Attendance class implements Calculations interface
+Calculates total attendance for a particular student*/
 
 import com.jevon.studentrollrecorder.interfaces.Calculations;
 import com.jevon.studentrollrecorder.pojo.Course;
 import com.jevon.studentrollrecorder.pojo.Session;
-
 import java.util.HashMap;
 
 public class Attendance implements Calculations{
 
-   // private HashMap<String, Session> sessions;
+  
     private Course course;
     private int totalSessions;
     private int totalSessionsNotAttended;
     private int totalSessionsAttended;
     private String studentId;
-
-   // private String course;
 
     public Attendance(){
 
@@ -23,10 +22,7 @@ public class Attendance implements Calculations{
     public void setStudentID(String studentId){
         this.studentId = studentId;
     }
-   /** public void setSessions(HashMap<String, Session> sessions){
-        this.sessions=sessions;
-
-    }//setSessions*/
+ 
     public void setCourse(Course course){
         this.course=course;
 
@@ -62,93 +58,6 @@ public class Attendance implements Calculations{
             }
         }//for loop
     }//end Calculate
-
-
-
-    // e.getSessions() != null && course.getLecturess() != null){
-    //    attendanceCalculations();
-   // }
-
-   /** public void attendanceCalculations(String courseCode){
-        this.firebaseHelper = new FirebaseHelper();
-
-        /* get a ref to the lecturer courses.
-        Firebase ref = firebaseHelper.getRef_id();
-
-        /* narrow down to the course we are interested in .
-        Firebase courseRef = ref.child(courseCode);
-
-        /* get the course from firebase and store it locally.
-
-        courseRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                /* load in the course.
-                course = dataSnapshot.getValue(Course.class);
-
-                if(course.getSessions() != null && course.getLecturess() != null){
-                    //attendanceCalculations();
-
-                    /*initialize variables.
-                    this.totalSessions  = 0;
-                    this.totalSessionsNotAttended = 0;
-                    this.totalSessionsAttended = 0;
-
-                    /* get the sessions from the course.
-                    HashMap<String, Session> sessions = course.getSessions();
-
-                    /* checks the total number of sessions stored in the hashmap.
-                    this.totalSessions =  sessions.size();
-
-                    /* iterate over each session.
-                    for(HashMap.Entry<String, Session> currentSession : sessions.entrySet()){
-
-                        /* check if the student we are interested in attended this session.
-                            totalSessionsAttended++;
-                        }
-                        else{
-                            totalSessionsNotAttended++;
-                        }
-
-
-                    }
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-                }
-
-        }
-                                        }*/
-      /**  courseRef.addValueEventListener(new ValueEventListener() {
-        /*initialize variables.
-        this.totalSessions  = 0;
-        this.totalSessionsNotAttended = 0;
-        this.totalSessionsAttended = 0;
-
-        /* get the sessions from the course.
-        HashMap<String, Session> sessions = course.getSessions();
-
-        /* checks the total number of sessions stored in the hashmap.
-        this.totalSessions =  sessions.size();
-
-        /* iterate over each session.
-        for(HashMap.Entry<String, Session> currentSession : sessions.entrySet()){
-
-            /* check if the student we are interested in attended this session.
-            if(currentSession.getValue().getAttendees().get(studentId) != null){
-                totalSessionsAttended++;
-            }
-            else{
-                totalSessionsNotAttended++;
-            }
-        }
-
-        updateAttendancePieChart();
-
-    }
-       }//end attendance calculations*/
-
 
 
 
